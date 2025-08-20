d5b8c14842f6c828823234bb4e83b287f80eca53
Koci1 <fejzagic.dino04@gmail.com>
2025-08-20 10:36:45 +0200

    Add websocket support

--- chat_app/asgi.py
+++ chat_app/asgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter,ProtocolTypeRouter
+from chat import routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        'http':get_asgi_application(),
+        'websocket': AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    }
+)
